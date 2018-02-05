d80e94fb76b4cdf7911d7975aade8245a10249bb
hnandana <36050067+hnandana@users.noreply.github.com>
2018-02-05 13:59:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python
-print("Hello World")
+"""A setuptools based setup module.
+
+"""
+from setuptools import setup, find_packages
+setup(
+    # This is the name of your project. The first time you publish this
+    # package, this name will be registered for you. It will determine how
+    # users can install this project, e.g.:
+    #
+    # $ pip install TestProject
+    #
+    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
+    #
+    # There are some restrictions on what makes a valid project name
+    # specification here:
+    # https://packaging.python.org/specifications/core-metadata/#name
+    name='TestProject',  # Required
+    version='1.2.0',  # Required
+    packages=find_packages(exclude=['Test']),  # Required
+)
